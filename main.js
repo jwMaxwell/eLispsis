31999624fb0ec17425fbd5fb5f113edc315a7cb0
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2021-10-05 19:17:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { exit } = require("process");
 const prompt = require("prompt-sync")();
 const fs = require("fs");
+console.error = (x) => console.log('\x1b[1m\x1b[31m' + x + '\x1b[0m');
 
 const tokenize = (str) =>
     `${str.trim()}`
@@ -35,55 +36,72 @@ const parseQuote = (node) =>
 
 const isAtom = (val) => !Array.isArray(val);// || !val.length();
 
+const find = (ctx, value) => {
+    if (value in ctx.scope)
+        return ctx.scope[value];
+    else if (ctx.parent === undefined) {
+        console.error(`Expression "${value}" not found`);
+        exit();
+    }
+    return find(ctx.parent, value);
+}
+
 const evaluate = (ast, ctx) => {
     console.log(ctx);
-    if (isAtom(ast) && ast in ctx) {
-        return ctx[ast];
+    if (isAtom(ast) && find(ctx, ast)) {
+        return find(ctx, ast);
     }
     else if (isAtom(ast)) {
         return ast;
     }
     else {
-        const func = evaluate(ast[0], ctx);
+        const func = evaluate(ast[0], { scope: {}, parent: ctx });
         if (!(func instanceof Function)) {
             console.error(`Expression "${func}" not found`);
             exit();
         }
-        return func(ast.slice(1), ctx);
+        return func(ast.slice(1), { scope: {}, parent: ctx });
     }
 }
 
 const core = {
-    'quote': ([x]) => x,
-    'atom': ([x], ctx) => isAtom(evaluate(x, ctx)) ? 't' : [],
-    'head': ([x], ctx) => evaluate(x, ctx)[0],
-    'tail': ([x], ctx) => evaluate(x, ctx).slice(1),
-    'push': ([x, y], ctx) => [evaluate(x, ctx), ...evaluate(y, ctx)],
-    'if': ([x, y, z], ctx) => evaluate(x, ctx) === 't'
-        ? evaluate(y, ctx)
-        : evaluate(z, ctx),
-    'var': ([name, value], ctx) => {
-        ctx[name] = evaluate(value)
-    },
-    'expr': ([args, body], ctx) => (x) => {
-        x = Object.values(Object(x));
-        const exprCtx = ctx;
-        for (let i = 0; i < args.length; ++i)
-            exprCtx[args[i]] = evaluate(x[i]);
-        return evaluate(body, exprCtx);
-    },
-    'func': ([name, args, body], ctx) => {
-        ctx[name] = ctx.expr([args, body], ctx);
-    },
+    'scope': {
+        'quote': ([x]) => x,
+        'atom': ([x], ctx) => isAtom(evaluate(x, ctx)) ? 't' : [],
+        'head': ([x], ctx) => evaluate(x, ctx)[0],
+        'tail': ([x], ctx) => evaluate(x, ctx).slice(1),
+        'push': ([x, y], ctx) => [evaluate(x, ctx), ...evaluate(y, ctx)],
+        'if': ([x, y, z], ctx) => evaluate(x, ctx) === 't'
+            ? evaluate(y, ctx)
+            : evaluate(z, ctx),
+        'var': ([name, value], ctx) => {
+            ctx.parent[name] = evaluate(value)
+            return [];
+        },
+        'expr': ([args, body], ctx) => (x) => {
+            x = Object.values(Object(x));
+            const exprCtx = ctx;
+            for (let i = 0; i < args.length; ++i)
+                exprCtx[args[i]] = evaluate(x[i]);
+            return evaluate(body, exprCtx);
+        },
+        'func': ([name, args, body], ctx) => {
+            // console.log(`funcname`);
+            // console.log(name);
+            ctx.parent[name] = ctx.expr([args, body], ctx);
+            // console.log(ctx[name]);
+            return [];
+        },
+    }
 };
 
 const execute = (expressions) =>
-      expressions.reduce((ctx, expr) => evaluate(expr, ctx), core);
+      expressions.reduce((ctx, expr) => evaluate(expr, ctx), { scope: {}, parent: core });
 
 const main = (() => {
     const input = prompt('lisp > ');
-//    console.log(evaluate(parseQuote(parse(tokenize(input))), core));
-    //console.log(parseQuote(parse(tokenize(input))));
+
+    console.log([parseQuote(parse(tokenize(input)))]);
     console.log(execute([parseQuote(parse(tokenize(input)))]));
 
 //    console.log(evaluate(parseQuote(parse(tokenize(input)))));
