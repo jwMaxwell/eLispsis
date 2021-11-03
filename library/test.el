d633d03688b006152eb8cbb1a47e64c9175db340
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2021-11-03 12:43:02 -0500

    patch?

--- library/test.el
+++ library/test.el
@@ -17,7 +17,10 @@
     ('t (binToInt (cdr s)))))
 
 (print (binToInt "1010001"))
-(print (% 324 3))
+(defun if (pred then else)
+  (cond (pred then)
+    ('t else)))
+(print (+ '1 (car "2a")))
 
 ;; a code challenge for you (language of your choice): given a string "S" of 1's and 0's representing a binary number, and a number N, return S mod N. S can be very large. N will be between 1 and a billion.
 ;; example: modulo('1010001', 13) would return 3
